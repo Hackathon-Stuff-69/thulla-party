5f171e6d772a4effc5284412ed6c432a73e7eab7
Mohammad Usman <46886908+mohammadusman666@users.noreply.github.com>
2021-09-18 02:49:42 +0500

    Work

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithPopup, GoogleAuthProvider, Auth } from 'firebase/auth';
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  setPersistence,
+  browserSessionPersistence,
+  Auth,
+  User,
+} from 'firebase/auth';
+// import axios from 'axios';
+// import axios from 'axios';
 // import { getAuth } from 'firebase/auth';
 // import { getFirestore } from 'firebase/firestore';
 // import { getAnalytics } from "firebase/analytics";
@@ -24,53 +34,76 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+console.log(app);
 // const analytics = getAnalytics(app);
 
-console.log(app);
-
-const provider = new GoogleAuthProvider();
-
-const auth = getAuth();
-
-const googleSignIn = (auth: Auth, provider: GoogleAuthProvider) =>
-  signInWithPopup(auth, provider)
-    .then((result) => {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      // const credential = GoogleAuthProvider.credentialFromResult(result);
-      // const token = credential.accessToken;
-      // The signed-in user info.
-      const user = result.user;
-      // ...
-      console.log(user);
-    })
-    .catch((error) => {
-      // Handle Errors here.
-      // const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      // const email = error.email;
-      // The AuthCredential type that was used.
-      // const credential = GoogleAuthProvider.credentialFromError(error);
-      // ...
-      console.log(errorMessage);
-    });
+type State = {
+  user: User | null;
+  rooms: [];
+};
 
 const App: React.FC = () => {
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth();
+  const [state, setState] = useState<State>({ user: auth.currentUser, rooms: [] });
+
+  const googleSignIn = (auth: Auth, provider: GoogleAuthProvider) =>
+    setPersistence(auth, browserSessionPersistence)
+      .then(() => {
+        // Existing and future Auth states are now persisted in the current
+        // session only. Closing the window would clear any existing state even
+        // if a user forgets to sign out.
+        // ...
+        // New sign-in will be persisted with session persistence.
+        return signInWithPopup(auth, provider).then((result) => {
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          // const credential = GoogleAuthProvider.credentialFromResult(result);
+          // const token = credential.accessToken;
+          // The signed-in user info.
+          setState((prevState) => ({ ...prevState, user: result.user }));
+        });
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        // const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorMessage);
+      });
+
+  console.log(state);
+
+  useEffect(() => {
+    // if (state.user?.uid)
+    // axios()
+  }, [state.user]);
+
   return (
     <Router>
+      <Switch>
+        {/* <Route exact path='/todos'>
+            <Todos />
+          </Route> */}
+      </Switch>
+
       <div>
         <nav className='flex items-center justify-between flex-wrap bg-green-500 p-6'>
           <div className='flex items-center flex-shrink-0 text-white mr-6'>
             <span className='font-semibold text-xl tracking-tight'>Thulla Party</span>
           </div>
-          <button onClick={() => googleSignIn(auth, provider)}>Login Google</button>
+          {state.user ? (
+            <div>{state.user.displayName}</div>
+          ) : (
+            <button className='text-white' onClick={() => googleSignIn(auth, provider)}>
+              Login Google
+            </button>
+          )}
         </nav>
 
-        <Switch>
-          {/* <Route exact path='/todos'>
-            <Todos />
-          </Route> */}
-        </Switch>
+        {state.user && (
+          <div className='flex flex-col'>
+            <button>Create Room</button>
+          </div>
+        )}
       </div>
     </Router>
   );
