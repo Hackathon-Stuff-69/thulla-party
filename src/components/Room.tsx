764ed25a66bce9416a4779c789593543f40bd3cf
Mohammad Usman <46886908+mohammadusman666@users.noreply.github.com>
2021-09-18 16:13:10 +0500

    Add vscode settings.json and remove dead code

--- src/components/Room.tsx
+++ src/components/Room.tsx
@@ -7,17 +7,11 @@ import DailyIframe, { DailyCall } from '@daily-co/daily-js';
 import { DAILY_API_HEADERS, RoomItem } from './../constants';
 
 type State = {
-  isScreenSharing: boolean;
   room?: RoomItem;
 };
 
-// https://boriat-bhagao.daily.co/7NtCkTxsgu3EhEgX3VgC
-
 const Room = ({ user }: { user: User | null }) => {
-  const [state, setState] = useState<State>({
-    isScreenSharing: false,
-    room: { name: 'yolo', url: 'https://boriat-bhagao.daily.co/7NtCkTxsgu3EhEgX3VgC' },
-  });
+  const [state, setState] = useState<State>();
   const callWrapperRef = useRef(null);
   const callFrame = useRef<DailyCall>();
   const { roomName } = useParams<{ roomName: string }>();
@@ -61,57 +55,12 @@ const Room = ({ user }: { user: User | null }) => {
         .catch(() => <Redirect to='/' />);
   };
 
-  const toggleCamera = () => {
-    if (callFrame.current) callFrame.current.setLocalVideo(!callFrame.current.participants().local.video);
-  };
-
-  const toggleMic = () => {
-    if (callFrame.current) callFrame.current.setLocalAudio(!callFrame.current.participants().local.audio);
-  };
-
-  const toggleScreenShare = () => {
-    if (callFrame.current) {
-      const callFrameTemp = callFrame.current;
-      const participants = callFrameTemp.participants();
-      if (participants.local) {
-        if (!participants.local.screen) callFrameTemp.startScreenShare();
-        else if (participants.local.screen) callFrameTemp.stopScreenShare();
-
-        setState((prevState) => ({ ...prevState, isScreenSharing: !prevState.isScreenSharing }));
-      }
-    }
-  };
-
   return (
     <div className='flex h-screen w-screen'>
       {state.room?.name ? (
         <>
           <div ref={callWrapperRef} className='w-1/4' />
           <div className='flex items-center justify-center w-3/4'>Render Game</div>
-          <div id='call-controls-wrapper' className='call-controls-wrapper hide'>
-            <div className='call-controls'>
-              <div className='controls-header'>
-                <h3>Call overview</h3>
-                <p id='demo-countdown' className='demo-countdown'></p>
-              </div>
-              <div className='controls-custom-example'>
-                <div className='custom-buttons'>
-                  <button onClick={toggleCamera} className='button controls-button white-button'>
-                    Toggle camera
-                    <img src='/assets/camera_icon.png' className='control-icon' alt='Drawing of a camera' />
-                  </button>
-                  <button onClick={toggleMic} className='button controls-button white-button'>
-                    Toggle mic
-                    <img src='/assets/mic_icon.png' className='control-icon' alt='Drawing of a microphone' />
-                  </button>
-                  <button onClick={toggleScreenShare} className='button controls-button white-button'>
-                    {state.isScreenSharing ? 'Stop Screen Share' : 'Start Screen Share'}
-                    <img src='/assets/screenshare_icon.png' className='control-icon' alt='Drawing of computer screen' />
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
         </>
       ) : (
         <div className='flex items-center justify-center w-full'>Loading Room</div>
