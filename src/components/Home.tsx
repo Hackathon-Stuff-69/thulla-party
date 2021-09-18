b444ad3e0b8debe0c624e7f0e02883b06eda7a36
Mohammad Usman <46886908+mohammadusman666@users.noreply.github.com>
2021-09-18 22:43:38 +0500

    Add-input-for-room-name

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -12,56 +12,72 @@ import { initializeRoom } from '../Game Logic/getawayFuncs';
 
 import { DAILY_API_HEADERS, MainState, RoomItem } from './../constants';
 import { useState } from 'react';
-import { Button, Modal } from 'react-bootstrap';
 
-const dummyGenerator = async () => {
-  return { data: { name: 'Test Daily Room Name', url: 'test.com' } };
+type HomeState = {
+  show: boolean;
+  roomName: string;
+  error: string;
 };
 
 const Home = ({ state, setState }: { state: MainState; setState: (room: RoomItem) => void }) => {
-  const [show, setShow] = useState(false);
-  const [roomName, setRoomName] = useState('');
+  const [homeState, setHomeState] = useState<HomeState>({ show: false, roomName: '', error: null });
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
   const createRoom = () => {
-    axios
-      .post('https://api.daily.co/v1/rooms', JSON.stringify({ properties: { enable_chat: true } }), DAILY_API_HEADERS)
-      // dummyGenerator()
-      .then((response: any) => {
-        initializeRoom(response.data).then((res) => console.log('firebase add result: ', res));
-        console.log(response.data);
-        setState(response.data);
-      })
-      .catch((error) => console.error(error))
-      .finally(() => {
-        handleClose();
-      });
+    if (homeState.roomName)
+      axios
+        .post(
+          'https://api.daily.co/v1/rooms',
+          JSON.stringify({ name: homeState.roomName, properties: { enable_chat: true } }),
+          DAILY_API_HEADERS,
+        )
+        .then((response: any) => {
+          initializeRoom(response.data).then((res) => console.log('firebase add result: ', res));
+          console.log(response.data);
+          setState(response.data);
+        })
+        .catch((error) => {
+          console.error(error);
+          setHomeState((prevState) => ({ ...prevState, error: 'A room with this name already exists' }));
+        });
   };
 
   return (
     <div
       className='flex flex-col justify-center space-y-6 h-full w-screen'
       style={{
-        background: `url('static/banner.png')`,
-        backgroundSize: 'cover',
+        background: `url('static/cover.png')`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: '100%',
       }}
     >
       {state.user && (
         <>
-          <button
-            className='flex flex-col items-center justify-center mx-auto mt-6 w-64 h-24 bg-white rounded shadow-lg'
-            onClick={handleShow}
-          >
-            + Add Room
-          </button>
+          <form className='flex flex-col items-center justify-center mx-auto mt-6 w-64 h-24 bg-white rounded shadow-lg space-y-2'>
+            {homeState.error && <p className='m-0 text-red-500'>{homeState.error}</p>}
+            <input
+              className={`${homeState.error ? 'border border-solid border-red-500' : ''} focus:outline-none p-2`}
+              placeholder='Enter Room Name'
+              onChange={(e) => setHomeState((prevState) => ({ ...prevState, roomName: e.target.value }))}
+              required
+            />
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                setHomeState((prevState) => ({ ...prevState, error: null }));
+                createRoom();
+              }}
+              type='submit'
+            >
+              Add Room
+            </button>
+          </form>
 
           <div className='flex mx-auto max-w-5xl'>
             <div className='flex flex-wrap justify-center'>
-              {state.rooms.map((room, index) => (
+              {state.rooms.map((room) => (
                 <Link className='p-4' key={room.name} to={`/room/${room.name}`}>
                   <div className='flex items-center justify-center bg-white w-64 h-64 rounded-lg shadow-lg'>
-                    Room {index + 1}
+                    {room.name}
                   </div>
                 </Link>
               ))}
@@ -69,22 +85,6 @@ const Home = ({ state, setState }: { state: MainState; setState: (room: RoomItem
           </div>
         </>
       )}
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <input type='text' placeholder='Enter Room Name' className='input' />
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant='secondary' onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant='primary' onClick={createRoom}>
-            Create Room
-          </Button>
-        </Modal.Footer>
-      </Modal>
     </div>
   );
 };
