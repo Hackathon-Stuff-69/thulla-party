4305ac2089a53ecee357a4d63d94fdb0837631a4
harisnaseer191 <harisnaseer191@gmail.com>
2021-09-18 21:09:07 +0500

    added modal option

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -11,12 +11,19 @@ import axios from 'axios';
 import { initializeRoom } from '../Game Logic/getawayFuncs';
 
 import { DAILY_API_HEADERS, MainState, RoomItem } from './../constants';
+import { useState } from 'react';
+import { Button, Modal } from 'react-bootstrap';
 
 const dummyGenerator = async () => {
   return { data: { name: 'Test Daily Room Name', url: 'test.com' } };
 };
 
 const Home = ({ state, setState }: { state: MainState; setState: (room: RoomItem) => void }) => {
+  const [show, setShow] = useState(false);
+  const [roomName, setRoomName] = useState('');
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   const createRoom = () => {
     axios
       .post('https://api.daily.co/v1/rooms', JSON.stringify({ properties: { enable_chat: true } }), DAILY_API_HEADERS)
@@ -26,7 +33,10 @@ const Home = ({ state, setState }: { state: MainState; setState: (room: RoomItem
         console.log(response.data);
         setState(response.data);
       })
-      .catch((error) => console.error(error));
+      .catch((error) => console.error(error))
+      .finally(() => {
+        handleClose();
+      });
   };
 
   return (
@@ -41,10 +51,11 @@ const Home = ({ state, setState }: { state: MainState; setState: (room: RoomItem
         <>
           <button
             className='flex flex-col items-center justify-center mx-auto mt-6 w-64 h-24 bg-white rounded shadow-lg'
-            onClick={createRoom}
+            onClick={handleShow}
           >
-            Create Room
+            + Add Room
           </button>
+
           <div className='flex mx-auto max-w-5xl'>
             <div className='flex flex-wrap justify-center'>
               {state.rooms.map((room, index) => (
@@ -58,6 +69,22 @@ const Home = ({ state, setState }: { state: MainState; setState: (room: RoomItem
           </div>
         </>
       )}
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <input type='text' placeholder='Enter Room Name' className='input' />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant='secondary' onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant='primary' onClick={createRoom}>
+            Create Room
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
